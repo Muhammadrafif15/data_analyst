5808e28ea67fb1a6425b41f5dee15e77f268c8d9
Muhammad Rafif Rizqullah <143942727+Muhammadrafif15@users.noreply.github.com>
2025-08-31 21:43:30 +0700

    path file

--- Us_stock_commodity_analyst/Dashboard.py
+++ Us_stock_commodity_analyst/Dashboard.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="US Stock Commodity Analyst", layout="wide", initi
 
 st.sidebar.title("Menu Navigation")
 
-df = pd.read_parquet(US_Stock_Data_Cleaned.csv)
+df = pd.read_parquet("US_Stock_Data_Cleaned.csv")
 
 norm_df = df.copy()
 
